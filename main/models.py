2f9601f6e6bf0633fb39acdd1141af5257952a62
AJ Spicknall <44103084+AJSpicknall@users.noreply.github.com>
2026-02-08 18:35:53 -0700

    Capstone project 2-8-2026

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Videogames(models.Model):
+    uploaded_by = models.CharField(max_length=100)
+    favorite_video_game = models.CharField(max_length=200)
+    level_of_difficulty = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    fighting_a_feature = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.uploaded_by}, their favorite video game is {self.favorite_video_game} the Difficulty level out of 10 is: {self.level_of_difficulty}, and is Fighting a feature? {self.fighting_a_feature}"
